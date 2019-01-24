4942b9d0f5a2314fbf63c852146da59fba95b2a1
Karthik Kunjithapatham <karthikk@Karthiks-MacBook-Pro.local>
2019-01-25 10:44:04 +1100

    All vets and owners now displayed in respective index pages. Closes #13. Closes #14

--- spring-pet-web/src/main/java/com/org/kk/springpetclinic/controllers/OwnerController.java
+++ spring-pet-web/src/main/java/com/org/kk/springpetclinic/controllers/OwnerController.java
@@ -1,13 +1,25 @@
 package com.org.kk.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.org.kk.springpetclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
 	
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"/owners","/owners/index","/owners/index.html"})
-	public String listOwners() {
+	public String listOwners(Model model) {
+		
+		model.addAttribute("owners", ownerService.findAll());
 		return "/owners/index";
 	}
 

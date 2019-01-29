a894344bf0d35daa407317b05a56489b5920880b
karthikk <karthikk@10.10.10.130>
2019-01-29 15:40:06 +1100

    Fixed broken links. Closes #43

--- spring-pet-web/src/main/java/com/org/kk/springpetclinic/controllers/OwnerController.java
+++ spring-pet-web/src/main/java/com/org/kk/springpetclinic/controllers/OwnerController.java
@@ -22,5 +22,10 @@ public class OwnerController {
 		model.addAttribute("owners", ownerService.findAll());
 		return "/owners/index";
 	}
-
+	
+	@RequestMapping({"/owners/find","/owners/find.html","/oups"})
+	public String findOwners() {
+		return "underconstruction";
+		
+	}
 }

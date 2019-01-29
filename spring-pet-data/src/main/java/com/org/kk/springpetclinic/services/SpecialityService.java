7b7c3832ae351158bc8a420e7c61ee61ce9a4e88
karthikk <karthikk@10.10.10.130>
2019-01-29 14:10:16 +1100

    Added speciality service. Closes #29

---
+++ spring-pet-data/src/main/java/com/org/kk/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.org.kk.springpetclinic.services;
+
+import com.org.kk.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long>{
+
+}
---
+++ spring-pet-data/src/main/java/com/org/kk/springpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,45 @@
+package com.org.kk.springpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.org.kk.springpetclinic.model.Speciality;
+import com.org.kk.springpetclinic.services.SpecialityService;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapClass<Speciality, Long> implements SpecialityService{
+
+	@Override
+	public Set<Speciality> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public Speciality save(Speciality object) {
+		// TODO Auto-generated method stub
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Speciality object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+	
+	
+
+}

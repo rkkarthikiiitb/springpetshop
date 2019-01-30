5aac2b246c419fa303b61dcf7c3b9920853960ca
Karthik Kunjithapatham <karthikk@Karthiks-MacBook-Pro.local>
2019-01-30 15:38:24 +1100

    Added JPA base entity. Closes #33

--- spring-pet-data/src/main/java/com/org/kk/springpetclinic/model/BaseEntity.java
+++ spring-pet-data/src/main/java/com/org/kk/springpetclinic/model/BaseEntity.java
@@ -2,13 +2,19 @@ package com.org.kk.springpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 	
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 4366131078289362797L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long Id;
 
 	public Long getId() {

3e134540605a368a5bbfd946c1c65a46b57a655d
Karthik Kunjithapatham <karthikk@Karthiks-MacBook-Pro.local>
2019-01-30 16:16:14 +1100

    Added JPA entities for vets. Closes #35

--- spring-pet-data/src/main/java/com/org/kk/springpetclinic/model/Pet.java
+++ spring-pet-data/src/main/java/com/org/kk/springpetclinic/model/Pet.java
@@ -1,27 +1,34 @@
 package com.org.kk.springpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="pets")
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
 	@ManyToOne
-	@JoinColumn(name="type_id")
+	@JoinColumn(name = "type_id")
 	private PetType petType;
 	@ManyToOne
-	@JoinColumn(name="owner_id")
+	@JoinColumn(name = "owner_id")
 	private Owner owner;
-	@Column(name="birth_date")
+	@Column(name = "birth_date")
 	private LocalDate birthDate;
-	@Column(name="name")
+	@Column(name = "name")
 	private String name;
+
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="pet")
+	private Set<Visit> visits = new HashSet<>();
 
 	public PetType getPetType() {
 		return petType;
@@ -53,6 +60,14 @@ public class Pet extends BaseEntity {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 
 }
--- spring-pet-data/src/main/java/com/org/kk/springpetclinic/model/Visit.java
+++ spring-pet-data/src/main/java/com/org/kk/springpetclinic/model/Visit.java
@@ -2,12 +2,24 @@ package com.org.kk.springpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="visits")
 public class Visit extends BaseEntity {
 
 	private static final long serialVersionUID = 804064736247266206L;
 
+	@Column(name="date")
 	private LocalDate date;
+	@Column(name="description")
 	private String description;
+	@ManyToOne
+	@JoinColumn(name="pet_id")
 	private Pet pet;
 
 	public LocalDate getDate() {
